f70dac5742e042f967a414e65aabb1f3a771bbb4
Max Koon <k2@65.253.21.198.tigernet.wifi.dyn.clemson.edu>
2023-09-13 13:01:07 -0400

    feat: restyle home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
+import { space } from "postcss/lib/list";
 
 import { api } from "~/utils/api";
 
+const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 export default function Home() {
-  const { data, isLoading } = api.example.getNextMeeting.useQuery();
 
   return (
     <>
@@ -14,57 +16,98 @@ export default function Home() {
         <meta name="description" content="Build @clemson" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#d66604] to-[#2b1f14]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Clemson.build
-          </h1>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {isLoading ? "Loading..." : data ? "Next meeting: " + data.date.toLocaleString() + " " + data.location : "No next meeting"}
-            </p>
-            <AuthShowcase />
-          </div>
-        </div>
+      <main className="bg-zinc-950">
+        <Splash />
       </main>
     </>
   );
 }
 
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
+function Splash() {
+    const { data, isLoading } = api.example.getNextMeeting.useQuery();
 
-  const { data: meeting } = api.example.getNextMeetingLink.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {meeting ?
-        meeting.slidesUrl ? (
-          <>
-              <a target="_blank" href={meeting.slidesUrl}>
-                  <button
-                    className="rounded-full bg-orange-600 px-10 py-3 font-semibold text-white no-underline transition hover:bg-orange-500"
-                  >
-                    {"Slides"}
-                  </button>
-              </a>
-              <br />
-          </>
-        )
-        : null
-        : null}
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
+    return <div className="h-screen bg-[#0e1416]">
+        <div className="w-full h-full overflow-hidden z-10" style={{ perspective: "50rem" }}>
+            <div className="w-full h-full absolute z-20" style={{ background: "radial-gradient(ellipse at 50% 50%, rgba(14, 20, 22, 0) 0%, #0e1416 80%);" }}></div>
+            <div className="w-full h-[200%] " style={{
+                background: "linear-gradient(to right, rgba(0, 255, 255, 0.3) 1px, transparent 0), linear-gradient(to bottom, rgba(255, 255, 255, 0.3) 1px, transparent 0)",
+                backgroundSize: "45px 30px",
+                backgroundRepeat: "repeat",
+                transformOrigin: "100% 0 0",
+                animation: "play 10s linear infinite",
+                }}></div>
+            <div className="top-0 w-full h-full absolute flex flex-col items-center justify-center z-30">
+                <Logo />
+                <div className="mt-16">
+                    {!data ? "Loading..." : <MeetingTime date={data.date} location={data.location} />}
+                </div>
+            </div>
+        </div>
     </div>
-  );
+}
+
+function getTime(date: Date): string {
+    const timeParts = date.toLocaleTimeString().split(":");
+    const meridian = timeParts[2]?.split(" ")[1]?.toLowerCase();
+
+    const time = `${timeParts[0]}:${timeParts[1]}${meridian}`;
+    return time;
+}
+
+interface MeetingTimeProps {
+    date: Date,
+    location: string;
+}
+function MeetingTime({ date, location }: MeetingTimeProps) {
+    const { data: sessionData } = useSession();
+
+
+    const day = DAYS[date.getDay()];
+    const dateParts = date.toLocaleDateString().split("/");
+    const timeStart = getTime(date);
+    const timeEnd = getTime(new Date(date.getTime() + 1000 * 60 * 60));
+
+    return <div className="text-center font-mono">
+        <h4 className="text-gray-400 text-2xl">Next Meeting</h4>
+        <h2 className="text-orange-400 text-4xl mt-2">{`${day} (${dateParts[0]}/${dateParts[1]})`}</h2>
+        <h2 className="text-orange-400 text-5xl mt-2">{timeStart}-{timeEnd}</h2>
+        <h5 className="text-orange-600 mt-6 text-3xl">{"///"} <span className="mx-4 text-white">Room: {location}</span> {"///"}</h5>
+        <div className="mt-6">
+            {sessionData
+            ? <Member />
+            : <SignIn />
+            }
+        </div>
+    </div>
+}
+
+function SignIn() {
+    return <button
+    className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+    onClick={() => void signIn()}
+  >
+    {"Sign in"}
+  </button>
+}
+
+function Member() {
+    const { data } = api.example.getNextMeetingLink.useQuery();
+    return data ? data.slidesUrl ? <SlidesLink url={data.slidesUrl} /> : <span></span> : <span></span>;
+}
+
+interface SlidesLinkProps {
+    url: string;
+}
+function SlidesLink({ url }: SlidesLinkProps) {
+    return <a target="_blank" href={url}>
+          <button
+            className="rounded-full bg-orange-600 px-10 py-3 font-semibold text-white no-underline transition hover:bg-orange-500"
+          >
+            {"Slides"}
+          </button>
+      </a>
+}
+
+function Logo() {
+    return <div className="text-orange-600 text-4xl font-semibold text-center">CLEMSON<br /><div className="mt-6"><span className="bg-orange-600 text-white p-4 text-6xl">.BUILD</span></div></div>
 }
