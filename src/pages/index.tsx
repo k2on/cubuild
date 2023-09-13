b823ca6bc531b972c9eac06ec9caa435f934f1e8
Max Koon <k2@65.253.21.198.tigernet.wifi.dyn.clemson.edu>
2023-09-13 10:22:23 -0400

    feat: color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const meeting = api.example.getNextMeeting.useQuery();
+  const { data, isLoading } = api.example.getNextMeeting.useQuery();
 
   return (
     <>
@@ -14,14 +14,14 @@ export default function Home() {
         <meta name="description" content="Build @clemson" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#d66604] to-[#2b1f14]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Clemson.build
           </h1>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {meeting.data ? "Next meeting: " + meeting.data.date.toLocaleString() + " " + meeting.data.location : "No next meeting"}
+              {isLoading ? "Loading..." : data ? "Next meeting: " + data.date.toLocaleString() + " " + data.location : "No next meeting"}
             </p>
             <AuthShowcase />
           </div>
